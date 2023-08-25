108ef501424a3a06b5a13e703d7cd61c0969fb8b
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2023-08-26 00:22:56 +0900

    로딩 애니메이션 추가

--- TodoList/Controller/PetViewController.swift
+++ TodoList/Controller/PetViewController.swift
@@ -9,19 +9,29 @@ import UIKit
 
 class PetViewController: UIViewController {
     
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
     @IBOutlet weak var petImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadingIndicator.hidesWhenStopped = true
         loadRandomCatImage()
     }
     
     func loadRandomCatImage() {
+        loadingIndicator.startAnimating()
         guard let url = URL(string: "https://api.thecatapi.com/v1/images/search") else {
+            loadingIndicator.stopAnimating()
             return
         }
         
         URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            // defer: 함수의 실행이 끝나기 직전에 실행
+            defer {
+                DispatchQueue.main.async {
+                    self?.loadingIndicator.stopAnimating()
+                }
+            }
             if let error = error {
                 print("Error : \(error)")
                 return
